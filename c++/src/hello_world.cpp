714f6923db0d52ce8e1003fc476e937c1efdc509
IainBrookshaw <iain.j.brookshaw@gmail.com>
2019-06-10 13:50:28 -0700

    ib-c-code-example: created hello-world example

---
+++ c++/src/hello_world.cpp
@@ -0,0 +1,12 @@
+/**
+ * Demo Repo: Hello World C++ Program
+ *
+ * Copyright (c) 2019, Futurewei Inc. All Rights Reserved
+ * Iain Brookshaw
+ *
+ * \brief this is a simple program to illustrate c++ coding
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv) { std::cout << "Hello World!!" << std::endl; }
